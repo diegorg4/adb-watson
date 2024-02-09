51260e8f496dcfed7f337ff8e26e87f8efefd3e1
diegorg4 <diegoe.romang@gmail.com>
2024-02-09 17:15:40 -0600

    Added web channel integration

--- wa_webhook/pydantic_models/request_model.py
+++ wa_webhook/pydantic_models/request_model.py
@@ -16,9 +16,13 @@ class RequestModel(BaseModel):
     trip_type: Optional[str] = Field(default=None, validation_alias=AliasChoices('trip_type','tipo_viaje'), pattern=settings.TRIP_TYPE_PATTERN)
     leaving_price: Optional[float] = Field(default=None, validation_alias=AliasChoices('leaving_price','precio_ida'))
     returning_price: Optional[float] = Field(default=None, validation_alias=AliasChoices('returning_price','precio_vuelta'))
+    channel: Optional[str] = Field(default=None, validation_alias=AliasChoices('channel','canal'))
 
     customer_name: Optional[str] = Field(default=None, validation_alias=AliasChoices('customer_name','nombre_cliente'))
+    
+    # For PHONE channel, customer_email field is not sent from the webhook calls on Watson Assistant
     customer_email: Optional[str] = Field(default="diego.roman@adbansys.com", validation_alias=AliasChoices('customer_email','email_cliente'), pattern=settings.EMAIL_PATTERN)
+    
     customer_dn: Optional[str] = Field(default=None, validation_alias=AliasChoices('customer_dn','telefono_cliente'), pattern=settings.DN_PATTERN)
     
     wa_message: Optional[str] = Field(default=None, validation_alias=AliasChoices('wa_message','mensaje_wa'))
